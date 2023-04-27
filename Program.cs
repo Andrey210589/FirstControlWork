b50e5bb077525bb6d5186f8a5767f27fcc794a31
Andrey210589 <PolishhukAk@mail.ru>
2023-04-27 23:25:08 +0300

    Method Create,Show

--- Program.cs
+++ Program.cs
@@ -6,3 +6,25 @@
 // либо задать на старте выполнения алгоритма. 
 // При решении не рекомендуется пользоваться коллекциями,
 // лучше обойтись исключительно массивами.
+
+string[] CreateStringArray(int size) // Создаем массив строк, размером size. Строки вводятся с клавиатуры.
+{
+    string[] array = new string[size];
+    for (int i = 0; i < array.Length; i++)
+    {
+        Console.WriteLine("Input string No: " + (i + 1));
+        array[i] = Console.ReadLine();
+    }
+    return array;
+}
+
+void ShowArray(string[] array) //Вывод массива
+{
+    Console.Write("{ ");
+    for (int i = 0; i < array.Length; i++)
+    {
+        Console.Write(" " + array[i] + " ");
+    }
+    Console.Write(" }");
+    Console.WriteLine();
+}
